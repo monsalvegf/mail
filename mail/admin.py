24e4936944b7da8c711c21c81fba38561319da50
Fernando <monsalvegf@yahoo.com>
2024-05-01 21:22:08 +0200

    Algún detalle revisado.

--- mail/admin.py
+++ mail/admin.py
@@ -10,11 +10,17 @@ admin.site.register(User, UserAdmin)
 
 # Clase personalizada para el modelo Email
 class EmailAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'sender', 'timestamp', 'read', 'archived')  # Mostrar estos campos en la lista
+    list_display = ('get_id', 'subject', 'sender', 'timestamp', 'read', 'archived')  # Mostrar estos campos en la lista
     list_filter = ('archived', 'read', 'timestamp')  # Filtrar por estos campos
     search_fields = ('subject', 'body', 'sender__username', 'recipients__username')  # Buscar por estos campos
     raw_id_fields = ('sender', 'user')  # Mejorar la interfaz para campos con muchos registros
     filter_horizontal = ('recipients',)  # Mejorar la selección de muchos-a-muchos
 
+    def get_id(self, obj):
+        return obj.id
+    get_id.admin_order_field = 'id'  # Permite ordenar por este campo
+    get_id.short_description = 'ID'  # Texto para la cabecera de la columna
+
 # Registrando el modelo Email con la clase personalizada
 admin.site.register(Email, EmailAdmin)
+
